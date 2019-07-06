c4556ddf1bc07969c3fe634d475b9628ad650fc7
erikflores@gmail.com <erikfloresq@gmail.com>
2019-07-05 21:56:00 -0500

    Update FireServer I

--- FireServer1.js
+++ FireServer1.js
@@ -9,10 +9,17 @@ admin.initializeApp({
 var registrationToken = "fu_ZmjkcvVM:APA91bEm6JRtJh7KsHPQfuaoi2RZ-jY4tmw4oDDRf2rCLyc_PMHO365QEREpPjJv3zUchOoap3QAve5F2ftWV6F8_FqxOoNcYlJoKxfBAc3BU6UeVc8ZJzn0dDrFCH2BZwZyr3u4zVOc";
 
 var payload = {
+    token: registrationToken,
     notification: {
-        title: "FirebaseServer I",
-        body: "Firebase is a great platform!",
-      },
+      title: "FirebaseServer I",
+      body: "Firebase is a great platform!",
+    },
+    android :{
+      ttl: "86400s",
+      notification: {
+        click_action: "OPEN_ACTIVITY_1"
+      }
+    },
     apns: {
       payload: {
         aps: {
@@ -24,7 +31,11 @@ var payload = {
         },
       },
     },
-    token: registrationToken,
+    webpush: {
+      headers: {
+        Urgency: "high"
+      }
+    },
     data: {
       "otherData": "Extra demo data",
       "attachment-url":"https://imgur.com/a/TPD7VBP"
